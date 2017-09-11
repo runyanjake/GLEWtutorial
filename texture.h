65d10b25125cc8d01ec8a8d7c33bd4a533e227b9
Jacob Runyan <jmrunyan@ucsc.edu>
2017-09-11 16:51:20 -0700

    added texture module

--- texture.h
+++ texture.h
@@ -3,18 +3,29 @@
 #ifndef __TEXTURE_H__
 #define __TEXTURE_H__
 
+//NOTE: All textures loaded from hard disk using stbimage. other implementations out there.
+
 #include <string>
+
+#ifdef __APPLE__
+#include <OpenGL/gl3.h> 
+#include <OpenGL/gl3ext.h> 
+#else
+#include <GL/glew.h>
+#endif
 
 class Texture{
 private:
-	
+	GLuint m_texture;
 public:
-	Texture(); //default ctor override
+	Texture(const std::string& filename); //default ctor override
 	Texture(const Texture& m) = delete; //delete copy ctor
 	Texture(Texture&& m) = delete; //delete move ctor
 	Texture& operator=(const Texture& m) = delete; //delete copy opr
 	Texture& operator=(Texture&& m) = delete; //delete move opr
 	virtual ~Texture(); //default dtor override
+
+	void Bind(unsigned int unit); //1-31
 };
 
 
